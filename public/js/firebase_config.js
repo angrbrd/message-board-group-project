2470f0b98cd58bd8d18e7c526d37079e91b26805
Anna D Derbakova <hiopa16@gmail.com>
2017-03-27 23:35:29 -0400

    Pulling in Firebase config file

---
+++ public/js/firebase_config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCaB6XyVczZpuyP039MfC0XyqF4D98wpK8",
+	authDomain: "message-board-916c9.firebaseapp.com",
+	databaseURL: "https://message-board-916c9.firebaseio.com",
+	storageBucket: "message-board-916c9.appspot.com",
+	messagingSenderId: "532403711809"
+};
+firebase.initializeApp(config);
