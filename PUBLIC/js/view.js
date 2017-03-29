2e4bc343606ff3e8bbe7c9c7f1ef4b20b2be3885
tom c <tom c>
2017-03-28 20:52:01 -0400

    tab test

--- PUBLIC/js/view.js
+++ PUBLIC/js/view.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     postContainer.empty();
     var rowsToAdd = [];
     for (var i = 0; i < posts.length; i++) {
-      rowsToAdd.push(createNewRow(posts[i]));
+      rowsToAdd.push(createNewRow(posts[i],i));
     }
     postContainer.prepend(rowsToAdd);
   }
@@ -22,19 +22,38 @@ $(document).ready(function() {
     });
   }
 
-  function createNewRow(post) {
-    var newInputRow = $("<li>");
-    newInputRow.addClass("list-group-item post-item");
-    var newPostSpan = $("<span>");
-    newPostSpan.text(post.title);
-    newInputRow.append(newPostSpan);
-    var newPostInput = $("<input>");
-    newPostInput.attr("type", "text");
-    newPostInput.addClass("edit");
-    newPostInput.css("display", "none");
-    newInputRow.append(newPostInput);
-    newInputRow.data("post", post);
-    return newInputRow;
+  function createNewRow(post, i) {
+
+    var postID = post.id;
+    var postTitle = post.title;
+    var postBody = post.body;
+    var postVotes = post.votes;
+    var postPhoto = post.photolink;
+    var postCreated = moment(post.createdAt, "YYYYMMDD").fromNow();
+
+
+   var newInputRow = $(`<br><div class="col-md-9" id = "post_` + postID + `">
+                <div class="row">
+                <div class="col-md-2">
+                    <div align="center"><img src="upvote" class="tiny upvoteButton"> </div></img>
+                    <div class="mediumtext" align="center">   ` + postVotes + ` 
+                
+                    </div>
+                    <div align="center"><img src="downvote" class="tiny downvoteButton"> </div></img>
+                </div>
+                <div class="col-md-2">
+                    <img src="` + postPhoto + ` " class="picture">
+                </div>
+                <div class="col-md-8">
+                    <h4><a href="/api/posts/` + postID + `">` + postTitle + ` </a></h4>
+                <p id = "postBody"> ` + postBody + `
+                </p>
+                <p class="small">Created ` + postCreated + `</p>
+                </div>
+                </div> `);
+
+   return newInputRow;
+
   }
 
   $(function () {
