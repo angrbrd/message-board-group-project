404f86a7ce91e80f264c56492086812eae6b7f81
Anna D Derbakova <hiopa16@gmail.com>
2017-03-30 21:47:30 -0400

    Add / to public

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
-var db = require(path.join(__dirname, 'models'));
+var db = require(path.join(__dirname, '/models'));
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -36,7 +36,7 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Static directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes =============================================================
 
